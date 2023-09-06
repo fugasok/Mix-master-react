2f16acfea676023399f2b96d24aa33bb0c130b65
Tibor <tibor736@hotmail.com>
2023-09-07 01:20:46 +0300

    Navbar links created

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const HomeLayout = () => {
 	return (
-		<div>
-			<nav>HomeLayout</nav>
+		<>
+			<Navbar />
 			<Outlet />
-		</div>
+		</>
 	);
 };
 export default HomeLayout;
