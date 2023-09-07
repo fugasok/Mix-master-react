1a22deeb9252417fda394c992730d461bdebb413
Tibor <tibor736@hotmail.com>
2023-09-07 20:13:14 +0300

    added react query and cashing for 5 min

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,16 +6,32 @@ import SearchForm from '../components/SearchForm';
 const cocktailSearchUrl =
 	'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
+import { useQuery } from '@tanstack/react-query';
+
+const searchCocktailsQuery = (searchTerm) => {
+	return {
+		queryKey: ['search', searchTerm || 'all'],
+		queryFn: async () => {
+			const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+			return response.data.drinks;
+		},
+	};
+};
+
 export const loader = async ({ request }) => {
 	const url = new URL(request.url);
 
 	const searchTerm = url.searchParams.get('search') || '';
-	const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-	return { drinks: response.data.drinks, searchTerm };
+
+	return { searchTerm };
 };
 
 const Landing = () => {
-	const { drinks, searchTerm } = useLoaderData();
+	const { searchTerm } = useLoaderData();
+	const { data: drinks, isLoading } = useQuery(
+		searchCocktailsQuery(searchTerm)
+	);
+	if (isLoading) return <h4>Loading...</h4>;
 
 	return (
 		<>
